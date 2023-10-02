dc3fa13babe4dbeb1aebd6aea89b672e4aa5a77b
Lukas Hammer <lukashammerprivate@gmail.com>
2023-10-02 22:07:02 +0200

    fix: add missing inventory methods

--- src/main/java/com/github/discordrpc/epitomyui/Gui.java
+++ src/main/java/com/github/discordrpc/epitomyui/Gui.java
@@ -1,5 +1,6 @@
 package com.github.discordrpc.epitomyui;
 
+import com.github.discordrpc.epitomyui.utils.GuiSerializer;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryClickEvent;
@@ -7,10 +8,12 @@ import org.bukkit.event.inventory.InventoryDragEvent;
 import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryHolder;
+import org.bukkit.inventory.ItemStack;
 
 import javax.annotation.Nonnegative;
 import javax.annotation.Nonnull;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public abstract class Gui implements InventoryHolder {
@@ -29,13 +32,127 @@ public abstract class Gui implements InventoryHolder {
         this.interactables = new HashMap<>(type.getDefaultSize());
     }
 
+    /**
+     * Opens the GUI for the given player.
+     *
+     * @param player The player to open the GUI for
+     */
     public void open(Player player) {
         UIProvider.registerUI(player.getUniqueId(), this);
         player.openInventory(this.inventory);
     }
 
+    /**
+     * Fills the entire inventory with the given item.
+     *
+     * @param item The item to fill the inventory with
+     */
+    public void fill(@Nonnull GuiItem item) {
+        for (int slot = 0; slot < this.inventory.getSize(); slot++) {
+            this.interactables.put(slot, item);
+            this.inventory.setItem(slot, item.getItem());
+        }
+    }
+
+    /**
+     * Fills the entire inventory with the given item.
+     *
+     * @param item The item to fill the inventory with
+     */
+    public void fill(ItemStack item) {
+        for (int slot = 0; slot < this.inventory.getSize(); slot++) {
+            this.inventory.setItem(slot, item);
+        }
+    }
+
+    /**
+     * Fills all empty slots in the inventory with the given item.
+     *
+     * @param item The item to fill the empty slots with
+     */
+    public void fillEmpty(@Nonnull GuiItem item) {
+        for (int slot = 0; slot < this.inventory.getSize(); slot++) {
+            if (this.inventory.getItem(slot) != null) continue;
+            this.interactables.put(slot, item);
+            this.inventory.setItem(slot, item.getItem());
+        }
+    }
+
+    /**
+     * Fills all empty slots in the inventory with the given item.
+     *
+     * @param item The item to fill the empty slots with
+     */
+    public void fillEmpty(@Nonnull ItemStack item) {
+        for (int slot = 0; slot < this.inventory.getSize(); slot++) {
+            if (this.inventory.getItem(slot) != null) continue;
+            this.inventory.setItem(slot, item);
+        }
+    }
+
+    /**
+     * Sets the given slots to the given item.
+     *
+     * @param slotStr The string of slots to set
+     * @param item The item to set
+     */
+    public void setItem(final String slotStr, @Nonnull GuiItem item) {
+        final List<Integer> slots = GuiSerializer.parseSlotString(slotStr);
+        for (final int slot : slots) {
+            setItem(slot, item);
+        }
+    }
+
+    /**
+     * Sets the given slot to the given item.
+     *
+     * @param slot The slot to set
+     * @param item The item to set
+     */
+    public void setItem(@Nonnegative final int slot, @Nonnull GuiItem item) {
+        if (slot >= inventory.getSize()) return;
+        this.interactables.put(slot, item);
+        this.inventory.setItem(slot, item.getItem());
+    }
+
+    /**
+     * Sets the given slots to the given item.
+     *
+     * @param slotStr The string of slots to set
+     * @param item The item to set
+     */
+    public void setItem(final String slotStr, ItemStack item) {
+        final List<Integer> slots = GuiSerializer.parseSlotString(slotStr);
+        for (final int slot : slots) {
+            setItem(slot, item);
+        }
+    }
+
+    /**
+     * Sets the given slot to the given item.
+     *
+     * @param slot The slot to set
+     * @param item The item to set
+     */
+    public void setItem(@Nonnegative final int slot, ItemStack item) {
+        if (slot >= inventory.getSize()) return;
+        this.inventory.setItem(slot, item);
+    }
+
+    /**
+     * Called when an item in the inventory is clicked.
+     *
+     * @param event The {@link InventoryClickEvent}
+     * @return True if the event should be cancelled, otherwise false
+     */
     public abstract boolean onClick(InventoryClickEvent event);
 
+    /**
+     * Called when an item in the inventory is dragged.
+     *
+     * @param event The {@link InventoryDragEvent}
+     * @return True if the event should be cancelled, otherwise false
+     */
     public abstract boolean onDrag(InventoryDragEvent event);
 
     @Override
@@ -45,5 +162,16 @@ public abstract class Gui implements InventoryHolder {
 
     public @Nonnull Map<Integer, GuiItem> getInteractables() {
         return this.interactables;
+    }
+
+    public @Nonnull Map<Integer, ItemStack> getItems() {
+        final int size = this.inventory.getSize();
+        final Map<Integer, ItemStack> items = new HashMap<>(size);
+        for (int slot = 0; slot < size; slot++) {
+            ItemStack item = this.inventory.getItem(slot);
+            if (item == null) continue;
+            items.put(slot, item);
+        }
+        return items;
     }
 }
---
+++ src/main/java/com/github/discordrpc/epitomyui/utils/GuiSerializer.java
@@ -0,0 +1,71 @@
+package com.github.discordrpc.epitomyui.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GuiSerializer {
+
+    /**
+     * Parses a slot string, e.g. "0-8,9,17,18-26", to a {@link List}
+     * of integers.
+     *
+     * @param str The slot string to parse
+     * @return A {@link List} of all slots
+     *
+     * @author <a href="https://github.com/hamza-cskn/">Hamza Coşkun</a>
+     * @author <a href="https://github.com/discordrpc">Lukas Hammer</a>
+     */
+    public static List<Integer> parseSlotString(String str) {
+        if (str == null) return new ArrayList<>();
+        str = str.replaceAll(" ", "");
+        if (str.contains(",")) {
+            return parseStringAsIntegerList(str);
+        } else if (str.contains("-")) {
+            return parseStringAsIntegerRange(str);
+        }
+        return new ArrayList<>();
+    }
+
+    /**
+     * @author <a href="https://github.com/hamza-cskn/">Hamza Coşkun</a>
+     */
+    private static List<Integer> parseStringAsIntegerRange(final String str) {
+        final String[] slots = str.split("-");
+        if (slots.length != 2) return new ArrayList<>();
+        int from, to;
+
+        try {
+            from = Integer.parseInt(slots[0]);
+            to = Integer.parseInt(slots[1]);
+        } catch (NumberFormatException ignore) {
+            return new ArrayList<>();
+        }
+
+        if (from > to) return new ArrayList<>();
+
+        final List<Integer> result = new ArrayList<>();
+        while (from <= to) result.add(from++);
+        return result;
+    }
+
+    /**
+     * @author <a href="https://github.com/hamza-cskn/">Hamza Coşkun</a>
+     * @author <a href="https://github.com/discordrpc">Lukas Hammer</a>
+     */
+    private static List<Integer> parseStringAsIntegerList(final String str) {
+        final List<Integer> pageSlots = new ArrayList<>();
+        final String[] slotStrings = str.split(",");
+
+        for (final String slotText : slotStrings) {
+            try {
+                if (slotText.contains("-")) {
+                    pageSlots.addAll(parseStringAsIntegerRange(slotText));
+                } else {
+                    pageSlots.add(Integer.parseInt(slotText));
+                }
+            } catch (NumberFormatException ignore) {
+            }
+        }
+        return pageSlots;
+    }
+}
