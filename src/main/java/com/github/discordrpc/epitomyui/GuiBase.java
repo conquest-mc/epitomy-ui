efc60764faf8f5df50b30f44e0b21f8afff829c0
Lukas Hammer <lukashammerprivate@gmail.com>
2023-11-07 21:30:39 +0100

    docs(javadoc): improve and add missing JavaDoc notations
    Resolves #5

--- src/main/java/com/github/discordrpc/epitomyui/GuiBase.java
+++ src/main/java/com/github/discordrpc/epitomyui/GuiBase.java
@@ -12,6 +12,7 @@ import org.bukkit.inventory.ItemStack;
 
 import javax.annotation.Nonnegative;
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -20,14 +21,16 @@ public abstract class GuiBase implements InventoryHolder {
     protected final Map<Integer, GuiItem> interactables;
 
     @SuppressWarnings("deprecation")
-    public GuiBase(String title, @Nonnegative int rows) {
-        this.inventory = Bukkit.createInventory(this, rows * 9, title);
+    public GuiBase(@Nullable final String title, @Nonnegative int rows) {
+        if (title == null) this.inventory = Bukkit.createInventory(this, rows * 9);
+        else this.inventory = Bukkit.createInventory(this, rows * 9, title);
         this.interactables = new HashMap<>(rows * 9);
     }
 
     @SuppressWarnings("deprecation")
-    public GuiBase(@Nonnull InventoryType type, String title) {
-        this.inventory = Bukkit.createInventory(this, type, title);
+    public GuiBase(@Nonnull final InventoryType type, @Nullable final String title) {
+        if (title == null) this.inventory = Bukkit.createInventory(this, type);
+        else this.inventory = Bukkit.createInventory(this, type, title);
         this.interactables = new HashMap<>(type.getDefaultSize());
     }
 
@@ -36,7 +39,7 @@ public abstract class GuiBase implements InventoryHolder {
      *
      * @param player The player to open the GUI for
      */
-    public void open(Player player) {
+    public void open(@Nonnull final Player player) {
         UIProvider.registerUI(player.getUniqueId(), this);
         player.openInventory(this.inventory);
     }
@@ -47,7 +50,9 @@ public abstract class GuiBase implements InventoryHolder {
      * @param event The {@link InventoryClickEvent}
      * @return True if the event should be cancelled, otherwise false
      */
-    public abstract boolean onClick(InventoryClickEvent event);
+    public boolean onClick(@Nonnull final InventoryClickEvent event) {
+        return true;
+    }
 
     /**
      * Called when an item in the inventory is dragged.
@@ -55,17 +60,28 @@ public abstract class GuiBase implements InventoryHolder {
      * @param event The {@link InventoryDragEvent}
      * @return True if the event should be cancelled, otherwise false
      */
-    public abstract boolean onDrag(InventoryDragEvent event);
+    public boolean onDrag(@Nonnull final InventoryDragEvent event) {
+        return true;
+    }
 
+    /**
+     * @return The {@link Inventory} contained in the {@link InventoryHolder}
+     */
     @Override
-    public Inventory getInventory() {
+    public @Nonnull Inventory getInventory() {
         return this.inventory;
     }
 
-    public Map<Integer, GuiItem> getInteractables() {
+    /**
+     * @return The interactable {@link GuiItem}s in the Gui
+     */
+    public @Nonnull Map<Integer, GuiItem> getInteractables() {
         return this.interactables;
     }
 
+    /**
+     * @return All {@link ItemStack}s contained in the {@link Inventory}
+     */
     public @Nonnull Map<Integer, ItemStack> getItems() {
         final int size = this.inventory.getSize();
         final Map<Integer, ItemStack> items = new HashMap<>(size);
