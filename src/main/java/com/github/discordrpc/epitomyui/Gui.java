3f8568091e8d3cd506871bcc101468db4f3bd3bd
Lukas Hammer <lukashammerprivate@gmail.com>
2023-11-07 13:06:45 +0100

    docs(javadoc): improve parameter annotations

--- src/main/java/com/github/discordrpc/epitomyui/Gui.java
+++ src/main/java/com/github/discordrpc/epitomyui/Gui.java
@@ -6,17 +6,16 @@ import org.bukkit.inventory.ItemStack;
 
 import javax.annotation.Nonnegative;
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import java.util.List;
 
 public abstract class Gui extends GuiBase {
 
     public Gui(@Nullable final String title, @Nonnegative final int rows) {
-    public Gui(String title, @Nonnegative int rows) {
         super(title, rows);
     }
 
     public Gui(@Nonnull final InventoryType type, @Nullable final String title) {
-    public Gui(@Nonnull InventoryType type, String title) {
         super(type, title);
     }
 
@@ -25,7 +24,7 @@ public abstract class Gui extends GuiBase {
      *
      * @param item The item to fill the inventory with
      */
-    public void fill(@Nonnull GuiItem item) {
+    public void fill(@Nonnull final GuiItem item) {
         for (int slot = 0; slot < this.inventory.getSize(); slot++) {
             this.interactables.put(slot, item);
             this.inventory.setItem(slot, item.getItem());
@@ -37,7 +36,7 @@ public abstract class Gui extends GuiBase {
      *
      * @param item The item to fill the inventory with
      */
-    public void fill(ItemStack item) {
+    public void fill(@Nullable final ItemStack item) {
         for (int slot = 0; slot < this.inventory.getSize(); slot++) {
             this.inventory.setItem(slot, item);
         }
@@ -48,7 +47,7 @@ public abstract class Gui extends GuiBase {
      *
      * @param item The item to fill the empty slots with
      */
-    public void fillEmpty(@Nonnull GuiItem item) {
+    public void fillEmpty(@Nonnull final GuiItem item) {
         for (int slot = 0; slot < this.inventory.getSize(); slot++) {
             if (this.inventory.getItem(slot) != null) continue;
             this.interactables.put(slot, item);
@@ -61,7 +60,7 @@ public abstract class Gui extends GuiBase {
      *
      * @param item The item to fill the empty slots with
      */
-    public void fillEmpty(@Nonnull ItemStack item) {
+    public void fillEmpty(@Nonnull final ItemStack item) {
         for (int slot = 0; slot < this.inventory.getSize(); slot++) {
             if (this.inventory.getItem(slot) != null) continue;
             this.inventory.setItem(slot, item);
@@ -74,7 +73,7 @@ public abstract class Gui extends GuiBase {
      * @param slotStr The string of slots to set
      * @param item The item to set
      */
-    public void setItem(final String slotStr, @Nonnull GuiItem item) {
+    public void setItem(@Nonnull final String slotStr, @Nonnull final GuiItem item) {
         final List<Integer> slots = GuiSerializer.parseSlotString(slotStr);
         for (final int slot : slots) {
             setItem(slot, item);
@@ -87,7 +86,7 @@ public abstract class Gui extends GuiBase {
      * @param slot The slot to set
      * @param item The item to set
      */
-    public void setItem(@Nonnegative final int slot, @Nonnull GuiItem item) {
+    public void setItem(@Nonnegative final int slot, @Nonnull final GuiItem item) {
         if (slot >= inventory.getSize()) return;
         this.interactables.put(slot, item);
         this.inventory.setItem(slot, item.getItem());
@@ -99,7 +98,7 @@ public abstract class Gui extends GuiBase {
      * @param slotStr The string of slots to set
      * @param item The item to set
      */
-    public void setItem(final String slotStr, ItemStack item) {
+    public void setItem(@Nonnull final String slotStr, @Nullable final ItemStack item) {
         final List<Integer> slots = GuiSerializer.parseSlotString(slotStr);
         for (final int slot : slots) {
             setItem(slot, item);
@@ -112,7 +111,7 @@ public abstract class Gui extends GuiBase {
      * @param slot The slot to set
      * @param item The item to set
      */
-    public void setItem(@Nonnegative final int slot, ItemStack item) {
+    public void setItem(@Nonnegative final int slot, @Nullable final ItemStack item) {
         if (slot >= inventory.getSize()) return;
         this.inventory.setItem(slot, item);
     }
